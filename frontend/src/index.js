e01186ff2423e1d1f1cf720d0ea4ee4699238eb6
Mohammad Aslam <aslam071595@gmail.com>
2025-07-01 22:03:06 +0530

    fonts applied

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "./font.css";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
 import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -2,15 +2,15 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'Montserrat-VariableFont_wght, sans-serif',
+    fontFamily: 'Montserrat, sans-serif',
     h1: { fontFamily: 'Sarcolenta, sans-serif' },
     h2: { fontFamily: 'Sarcolenta, sans-serif' },
-    h3: { fontFamily: 'Sarcolenta, sans-serif' },
+    h: { fontFamily: 'Sarcolenta, sans-serif' },
     h4: { fontFamily: 'Sarcolenta, sans-serif' },
     h5: { fontFamily: 'Sarcolenta, sans-serif' },
     h6: { fontFamily: 'Sarcolenta, sans-serif' },
-    body1: { fontFamily: 'Montserrat-VariableFont_wght, sans-serif' },
-    body2: { fontFamily: 'Montserrat-VariableFont_wght, sans-serif' },
+    body1: { fontFamily: 'Montserrat, sans-serif' },
+    body2: { fontFamily: 'Montserrat, sans-serif' },
   },
 });
 
